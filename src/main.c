7c2a2aecd13a6c94a668e1d449d98d04a81631b3
DcruBro <info@dcrubro.com>
2026-08-27 20:26:34 +0200

    base logon

--- src/main.c
+++ src/main.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
+#include <string.h>
+#include <concord/discord.h>
+#include <concord/log.h>
+
+void on_ready(struct discord* client, const struct discord_ready* event) {
+    log_info("Logged in as %s", event->user->username);
+}
 
 int main(void) {
-    printf("Hello, World!\n");
+    ccord_global_init();
+
+    struct discord* client = discord_config_init("config.json");
+
+    discord_set_on_ready(client, &on_ready);
+    
+    discord_run(client);
+
+    discord_cleanup(client);
+    ccord_global_cleanup();
+    
     return 0;
 }
