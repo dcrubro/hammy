c7d2052facf5d535298cbfe7803b118518997900
DcruBro <info@dcrubro.com>
2026-09-03 15:42:58 +0200

    make percision conversion explicit in utils.c

--- src/hammy/utils.c
+++ src/hammy/utils.c
@@ -2,12 +2,12 @@
 
 void hammy_to_uppercase(char* str) {
     for (size_t i = 0; str[i] != '\0'; i++) {
-        str[i] = toupper((unsigned char)str[i]);
+        str[i] = (char)toupper((unsigned char)str[i]);
     }
 }
 
 void hammy_to_lowercase(char* str) {
     for (size_t i = 0; str[i] != '\0'; i++) {
-        str[i] = tolower((unsigned char)str[i]);
+        str[i] = (char)tolower((unsigned char)str[i]);
     }
 }
